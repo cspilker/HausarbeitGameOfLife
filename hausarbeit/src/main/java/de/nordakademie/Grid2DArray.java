866f239efbc441d294875da3d3b059e9d7214917
Cédric Spilker - I18 <cedric.spilker@nordakademie.de>
2020-10-18 17:56:47 +0200

    Add parameter to constructor of Grid2DArray

--- hausarbeit/src/main/java/de/nordakademie/Grid2DArray.java
+++ hausarbeit/src/main/java/de/nordakademie/Grid2DArray.java
@@ -3,10 +3,11 @@ package de.nordakademie;
 public class Grid2DArray extends Grid{
 
 
-    public Grid2DArray(int width, int height) {
+    public Grid2DArray(int width, int height, int[][] aliveCells) {
     super(width, height);
     }
 
-
-
+    public boolean getCellStatus(int xPos, int yPos) {
+        return false;
+    }
 }
