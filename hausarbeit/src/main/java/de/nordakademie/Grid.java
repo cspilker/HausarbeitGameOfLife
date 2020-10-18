3fe68b5eb26a376aff39fd21d873f076efd2fc63
Cédric Spilker - I18 <cedric.spilker@nordakademie.de>
2020-10-18 17:14:06 +0200

    Add empty abstract class Grid

---
+++ hausarbeit/src/main/java/de/nordakademie/Grid.java
@@ -0,0 +1,21 @@
+package de.nordakademie;
+
+
+public abstract class Grid {
+
+    public Grid(){
+
+    }
+
+
+    public int getWidth(){
+
+        return 0;
+    }
+
+
+    public int getHeight(){
+
+        return 0;
+    }
+}
